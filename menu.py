b67ec32bd74d050d440fe397f3c426deb8771c42
Your Name <you@example.com>
2024-07-16 01:26:59 -0500

    he creado un dataframe con indice de fechas

--- menu.py
+++ menu.py
@@ -1,8 +1,18 @@
 import numpy as np
 import pandas as pd
 
-dt=pd.to_datetime(['2024-07-03','2024-07-04'])
+dt=pd.to_datetime(['2024-07-03','2024-07-04','2021-01-01'])
 
-print(dt)
+df=pd.DataFrame({
+    'year':[2024,2025,2026],
+    'month':[1,2,3],
+    'day':[1,3,4]
+}, index=dt)
+
+print(type(df))
+
+
+
+
 
 
